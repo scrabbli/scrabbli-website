efd13d8fc2daf20bfd95f9f733a77af421aa01f0
kristenmills <kristen@kristen-mills.com>
2013-05-10 11:08:00 -0400

    did some things

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 require 'sinatra'
+require 'active_support/core_ext'
 require 'sinatra/simple-navigation'
 require 'simple-navigation-bootstrap'
+require 'padrino-helpers'
+# require 'padrino-core/application/rendering'
 
-register Sinatra::SimpleNavigation
+# layout 'layout.erb'
+
+set :partial_template_engine, :erb
+enable :partial_underscores
+
+register Padrino::Helpers
+# register Padrino::Rendering
+
 get '/' do
-  erb :index
+  render :erb, :index
 end
 
 get '/assignments' do
-  erb :assignments
+  render :erb, :assignments
 end
 
 get '/rules' do
-  erb :rules
+  render :erb, :rules
 end
